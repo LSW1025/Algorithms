ebf6db4fc81ac0aea13b2c3b6e2d524dc3c0d938
LSW1025 <coms3319@naver.com>
2016-02-13 18:27:09 +0900

    Create 1786.cpp

---
+++ 1786.cpp
@@ -0,0 +1,54 @@
+/*
+  KMP..
+*/
+#include<cstdio>
+#include<cstring>
+#include<vector>
+using namespace std;
+#define MAX 1000001
+ 
+char T[MAX], P[MAX];
+int K[MAX];
+int cnt;
+vector<int> res;
+ 
+int main(){
+    gets(T); gets(P);
+ 
+    int i = 0;
+    int j = 0;
+    while (P[++i]){ // Find the KMP table
+        if (P[i] == P[j])
+            K[i] = ++j;
+        else if (j)
+            j = K[j - 1];
+        else
+            K[i] = 0;
+    }
+ 
+    i = 0, j = 0;
+    int len = strlen(P);
+    while (T[i]){ // KMP Algorithm
+        if (T[i] == P[j]){
+            j++;
+            if (j == len){
+                cnt++;
+                res.push_back(i - len + 2);
+                j = K[j - 1];
+            }
+            i++;
+        }
+        else if (j)
+            j = K[j - 1];
+        else{
+            j = 0;
+            i++;
+        }
+    }
+ 
+    printf("%d\n", cnt);
+    for (int i = 0; i < res.size(); i++)
+        printf("%d ", res[i]);
+ 
+    return 0;
+}
